b225c2bdefeb430ca5584b183501fe2d513be083
hyun-pm <cn_a@naver.com>
2025-07-18 10:54:25 +0900

    Update route.ts

--- app/api/recommend/route.ts
+++ app/api/recommend/route.ts
@@ -8,10 +8,12 @@ export async function POST(req: Request) {
   const { weather } = await req.json();
   const prompt = createLookPrompt(weather);
   if (!prompt) throw new Error("Prompt is null");
+
   const res = await openai.chat.completions.create({
     model: "gpt-4o",
-    messages: [{ role: "user", content: prompt }],
+    messages: [{ role: "user", content: prompt as string }],
   });
+
   const json = JSON.parse(res.choices[0].message.content);
   return NextResponse.json(json);
 }
